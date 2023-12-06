ca60c6b9e0810945c116c781cfa4fe94649f0188
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-12-06 11:43:25 +0100

    Modify persone.rb file

--- persone.rb
+++ persone.rb
@@ -27,7 +27,39 @@ class Person < Nameable
   end
 
   def correct_name
-    @name
+    @namearequire_relative 'nameable'
+
+    class Person < Nameable
+      attr_accessor :name, :age, :rentals
+      attr_reader :id
+    
+      def initialize(age, name = 'Unknown', parent_permission: true)
+        super()
+        @id = rand(1_000_000)
+    
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
+        @rentals = []
+      end
+    
+      def can_use_services?
+        of_age? || @parent_permission == true
+      end
+    
+      def correct_name
+        @name
+      end
+    
+      def of_age?
+        @age >= 18
+      end
+    
+      def add_rentals(date, book)
+        @rentals.push(Rental.new(date, self, book)) unless @rentals.include?(Rental.new(date, self, book))
+      end
+    end
+    
   end
 
   def add_rental(book, date)
