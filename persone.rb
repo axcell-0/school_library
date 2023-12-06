bea1de255c6039deff7465693c548a0081ceafa1
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-12-06 10:49:56 +0100

    Fix linters errors

--- persone.rb
+++ persone.rb
@@ -1,8 +1,8 @@
 require_relative 'nameable'
 
 class Person < Nameable
-    attr_accessor :name, :age, :rentals
-    attr_reader :id
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
